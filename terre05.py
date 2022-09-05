d72c0c28e48418a60a1a352fbe4de313bed7f35f
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-05 15:08:45 +0200

    Create terre05.py

---
+++ terre05.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 10:26:18 2022
+
+@author: patry
+
+
+"""
+while True:
+ try:
+  nombre = int(input())
+  break
+ except ValueError:
+  print("Tu nous la metrra pas a l'envers") 
+nombre2=nombre%2 
+if nombre2 == 0 :
+    print('pair')
+else :
+    print('impair')
