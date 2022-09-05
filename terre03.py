1e57435ee2ce26c2290e520d6fcbaaa7b74fe59c
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-05 15:07:47 +0200

    Create terre03.py

---
+++ terre03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 14:26:07 2022
+
+@author: patry
+"""
+import sys
+
+n= len(sys.argv)
+
+for i in range (1,n):
+    print(sys.argv[i])
