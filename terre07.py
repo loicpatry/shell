f9f116d2d1de529f54e69e11f45700c03ab60611
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-05 18:05:45 +0200

    Update terre07.py

--- terre07.py
+++ terre07.py
@@ -8,10 +8,11 @@ Created on Mon Sep  5 17:37:35 2022
 import sys 
 i=0
 while i==0:
+    if len(sys.argv) >2:
+        print('Veuillez entrer une seule chaine de caractère')
+        break
     string=sys.argv[1]
     reverse_string= string[::-1]
     print(reverse_string)
     i=i+1
-    if len(sys.argv) >2:
-        print('Veuillez entrer une seule chaine de caractère')
-        break
+    
