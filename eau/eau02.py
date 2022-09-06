dbb4bfc01667c8f8a88d63efd470f8f63316468b
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 22:48:09 +0200

    Update eau02.py

--- eau/eau02.py
+++ eau/eau02.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 23:30:39 2022
 
+@author: patry
+"""
+array1 =[0,1,2,3,4,5,6,7,8,9]
+array2 =[0,1,2,3,4,5,6,7,8,9]
+array3 =[0,1,2,3,4,5,6,7,8,9]
+array4 =[0,1,2,3,4,5,6,7,8,9]
+
+for a in array1:
+    for b in array2:
+        for c in array3:
+            for d in array4:
+                if str(a+b) != str(c+d) and int(a+b)<int(c+d):
+                    print(str(a)+str(b)+' '+str(c)+str(d)+',' ,end=' ')
+print('')
