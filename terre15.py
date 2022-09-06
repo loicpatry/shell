b5eb5ef3c12090e6e0dba60b169836fc8f33f189
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 12:12:11 +0200

    Create terre15.py

---
+++ terre15.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 11:52:55 2022
+
+@author: patry
+"""
+import sys 
+
+n = len(sys.argv)
+
+try :
+    for i in range(1,n):   
+        nombre = int(sys.argv[i])
+except:
+    sys.exit('error')
+else:
+    s=1
+    while s<n-1:
+        if sys.argv[s]>sys.argv[s+1]:
+            sys.exit('Pas trié!')
+        s=s+1
+    sys.exit('Trié!')   
+    
