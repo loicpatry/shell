5c2148a474359b870f3af8c83a1a968269d2b66c
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 22:48:57 +0200

    Create eau03.py

---
+++ eau/eau03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 14:26:07 2022
+
+@author: patry
+"""
+import sys
+
+n= len(sys.argv)
+
+for i in range (n-1,0,-1):
+    print(sys.argv[i])
