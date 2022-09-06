99cedb741401381099e399d1a28e484473a42cec
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 23:02:35 +0200

    Create eau05.py

---
+++ eau/eau05.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 00:01:27 2022
+
+@author: patry
+"""
+import sys
+
+nombre_premier_list =[]
+liste_de_nombre=[]
+nombre=int(sys.argv[1])
+for i in range(0,nombre*10):
+    liste_de_nombre.append(i)
+    
+for l in liste_de_nombre:
+    for d in range(2,int(l/2+1)):
+        if l%d==0 :
+            break
+    else:
+        nombre_premier_list.append(l)
+b=0
+while b<200000:
+    try:
+        index = nombre_premier_list.index(nombre)
+    except:
+        nombre=nombre+1
+    else:
+        print(nombre_premier_list[index])
+        sys.exit()
+    b=b+1
