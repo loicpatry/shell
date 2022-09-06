1b7bfe03783e37385a9f73731ccfbac6311b4880
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 16:12:22 +0200

    Update terre01.py

--- terre01.py
+++ terre01.py
@@ -2,14 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 23 23:02:33 2022
-
 @author: patry
 """
 
 alphabet  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 for i in range(0,26) : 
-    if i == 25 : 
-        print('')
-    else :     
-        print(alphabet[i],end='')
+    print(alphabet[i],end='')
     i= i+1 
