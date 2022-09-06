d7bf5d12a1967852b3e627f8bfa3ee1a4f2348dc
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 11:50:31 +0200

    Create terre12.py

---
+++ terre12.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 20:21:45 2022
+
+@author: patry
+"""
+import sys 
+
+heure_et_minutes = sys.argv[1]
+
+heure= int(heure_et_minutes[0:2])
+minutes= int(heure_et_minutes[3:5])
+heure0= heure_et_minutes[0:2]
+minutes0= heure_et_minutes[3:5]
+
+if  0<heure<12:
+    print(str(heure0)+':'+str(minutes0)+'AM')        
+elif heure==12:
+    print(str(heure0)+':'+str(minutes0)+'PM')
+elif 12< heure < 24:
+    heure=heure-12
+    print(str(heure)+':'+str(minutes0)+'PM')
+else : 
+    print('00:00AM')
+    
