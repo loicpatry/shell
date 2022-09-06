0d7c3adbbfee4ecf014df4c5f2eebb6aaf88d46d
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 15:27:39 +0200

    Update terre08.py

--- terre08.py
+++ terre08.py
@@ -7,15 +7,16 @@ Created on Mon Sep  5 17:37:35 2022
 """
 import sys 
 
-if len(sys.argv) >2:
+
+if sys.argv.__len__() >2:
     sys.exit('Veuillez entrer une seule chaine de caractère')
-if len(sys.argv) ==1:
+if sys.argv.__len__() ==1:
     sys.exit('Veuillez entrer une chaine de caractère')   
 try:
     int(sys.argv[1])
 except:
     string=sys.argv[1]
-    print(len(string))  
+    print(string.__len__())  
 else:
     sys.exit('error')
     
