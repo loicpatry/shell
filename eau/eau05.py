08cd4dd4a3f3a1027c3cbedd2b29f771ce58b38f
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 23:05:48 +0200

    Update eau05.py

--- eau/eau05.py
+++ eau/eau05.py
@@ -7,25 +7,32 @@ Created on Tue Sep  6 00:01:27 2022
 """
 import sys
 
-nombre_premier_list =[]
-liste_de_nombre=[]
-nombre=int(sys.argv[1])
-for i in range(0,nombre*10):
-    liste_de_nombre.append(i)
+try : 
+    nombre=int(sys.argv[1])
+except : 
+    sys.exit('error')
+else:
+    if nombre<0: 
+        sys.exit('-1')
+    nombre_premier_list =[]
+    liste_de_nombre=[]
+    nombre=int(sys.argv[1])
+    for i in range(0,nombre*10):
+        liste_de_nombre.append(i)
     
-for l in liste_de_nombre:
-    for d in range(2,int(l/2+1)):
-        if l%d==0 :
-            break
-    else:
-        nombre_premier_list.append(l)
-b=0
-while b<200000:
-    try:
-        index = nombre_premier_list.index(nombre)
-    except:
-        nombre=nombre+1
-    else:
-        print(nombre_premier_list[index])
-        sys.exit()
-    b=b+1
+    for l in liste_de_nombre:
+        for d in range(2,int(l/2+1)):
+            if l%d==0 :
+                break
+        else:
+            nombre_premier_list.append(l)
+    b=0
+    while b<200000:
+        try:
+            index = nombre_premier_list.index(nombre)
+        except:
+            nombre=nombre+1
+        else:
+            print(nombre_premier_list[index])
+            sys.exit()
+        b=b+1
