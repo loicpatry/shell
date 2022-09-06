ecab8284dac0aa367ee1d3035e8391cfd774dbf0
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 11:01:13 +0200

    Update terre04.py

--- terre04.py
+++ terre04.py
@@ -5,17 +5,15 @@ Created on Tue Aug 23 23:02:33 2022
 
 @author: patry
 """
-B=input()
+import sys 
+
+B = sys.argv[1]
 alphabet  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
-if B == 'a':
-    b=1
-if B == 'b':
-    b=2
-if B == 'c':
-    b=3
+
+b = alphabet.index(B)
 for i in range(b,26) : 
     if i == 25 : 
-        print('\n')
+        print('')
     else :     
         print(alphabet[i],end='')
     i= i+1 
