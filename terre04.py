965ee260d6f29ef02cbd2a7b14a91ccbc962c995
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 16:13:58 +0200

    Update terre04.py

--- terre04.py
+++ terre04.py
@@ -8,12 +8,10 @@ Created on Tue Aug 23 23:02:33 2022
 import sys 
 
 B = sys.argv[1]
-alphabet  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+alphabet  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 b = alphabet.index(B)
-for i in range(b,26) : 
-    if i == 25 : 
-        print('')
-    else :     
-        print(alphabet[i],end='')
-    i= i+1 
+for i in range(b,26) :
+    print(alphabet[i],end='')
+    i= i+1
+
