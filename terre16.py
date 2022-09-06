a01f8e45b045234eedb3b1d2d903b46267f13cec
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-06 16:05:17 +0200

    Create terre16.py

---
+++ terre16.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 16:04:01 2022
+
+@author: patry
+"""
+print('J\'ai terminé l\'épreuve de la terre et c\'était fructifiant')
