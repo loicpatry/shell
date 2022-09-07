6a70bcd4dbe030ba2db0f996a1cee0205b68af54
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-07 23:01:32 +0200

    Update eau04.py

--- eau/eau04.py
+++ eau/eau04.py
@@ -7,15 +7,20 @@ Created on Mon Sep  5 23:51:23 2022
 """
 import sys 
 
-
-N = int(sys.argv[1])
-if N<0: 
-    sys.exit('-1')
-fibonacciList = [0, 1]
-term = 2
-while term < N + 1:
-    value = fibonacciList[term - 1] + fibonacciList[term - 2]
-    fibonacciList.append(value)
-    term = term + 1
+if len(sys.argv) != 2:
+    sys.exit('veuillez entrer UN nombre')
+try :
+    N = int(sys.argv[1])
+except:
+    sys.exit('veuillez entrer un nombre')
+else: 
+    if N<0: 
+        sys.exit('-1')
+    fibonacciList = [0, 1]
+    term = 2
+    while term < N + 1:
+        value = fibonacciList[term - 1] + fibonacciList[term - 2]
+        fibonacciList.append(value)
+        term = term + 1
     
-print(fibonacciList[N])
+    print(fibonacciList[N])
