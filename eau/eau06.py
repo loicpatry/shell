6e0db8a7612c2bf9781a7217d0fcf8e73c4dd6a6
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-07 23:06:48 +0200

    Update eau06.py

--- eau/eau06.py
+++ eau/eau06.py
@@ -11,9 +11,15 @@ if sys.argv.__len__() <3:
     sys.exit('error')
 if sys.argv.__len__() >3:
     sys.exit('error')
-resultat=sys.argv[1].find(sys.argv[2])
+try: 
+    nombre1=int(sys.argv[1])
+    nombre2=int(sys.argv[2])
+except:
+    resultat=sys.argv[1].find(sys.argv[2])
 
-if resultat >0: 
-    print('True')
-if resultat == -1:
-    print('False')
+    if resultat >0: 
+        print('True')
+    if resultat == -1:
+        print('False')
+else:
+    sys.exit('error')
