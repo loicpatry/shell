087ab5ec71fa18a0f7a626819195d9798a317f73
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-08 09:54:57 +0200

    Create eau13.py

---
+++ eau/eau13.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 09:46:43 2022
+
+@author: patry
+"""
+import sys 
+
+n=len(sys.argv)
+
+try:
+    for q in range(1,n):
+        nombre=int(sys.argv[q])
+except: 
+    sys.exit('veuilez n\'entrez que des nombres')
+else:
+
+    mon_tableau=[]
+    
+    for m in range(1,n):
+        mon_tableau.append(int(sys.argv[m]))
+    
+    def my_bubble_sort(mon_tableau):
+        for i in range(n-1,1,-1):
+            for j in range(0,i-1):
+                if mon_tableau[j+1]<mon_tableau[j]:
+                    (mon_tableau[j+1],mon_tableau[j])=(mon_tableau[j],mon_tableau[j+1])
+        return(mon_tableau)
+    
+    my_bubble_sort(mon_tableau)
+    print(mon_tableau)
+    
