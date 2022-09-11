dac6da21e24e08b948c9132286c804adc60a52ee
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-11 19:34:15 +0200

    Create air04.py

---
+++ air/air04.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 22:42:14 2022
+
+@author: patry
+"""
+import sys 
+
+
+if len(sys.argv)==1:
+    sys.exit('error')
+for i in range(0,len(sys.argv)-1):
+    liste_arg=[]
+    for b in range(1,len(sys.argv)):
+        liste_arg.append(sys.argv[b])
+    liste_arg_ref=[]
+    for a in range(1,len(sys.argv)):
+        liste_arg_ref.append(sys.argv[a])
+    liste_arg.remove(liste_arg[i])
+    
+    if liste_arg_ref[i] in liste_arg:
+        continue
+    else:
+        print(liste_arg_ref[i])
