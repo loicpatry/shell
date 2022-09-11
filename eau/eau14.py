4b79fa9827ea4c5dd2dc0e0b228ae8a7d6682ae5
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-11 21:21:06 +0200

    Update eau14.py

--- eau/eau14.py
+++ eau/eau14.py
@@ -7,6 +7,22 @@ Created on Thu Sep  8 09:46:43 2022
 """
 import sys 
 
+#############################################################################
+
+def tri_selectif(mon_tableau):
+        l= len(mon_tableau)
+        for i in range(0,l-1):
+            mini=i
+            for j in range(i+1,l):
+                if mon_tableau[j]<mon_tableau[mini]:
+                    mini=j
+            if mini != i:
+                (mon_tableau[i],mon_tableau[mini])=(mon_tableau[mini],mon_tableau[i])
+        for element in mon_tableau:
+            print(element,end=' ')
+        print('')
+
+#############################################################################
 n=len(sys.argv)
 
 if n<3:
@@ -23,18 +39,4 @@ else:
     for m in range(1,n):
         mon_tableau.append(int(sys.argv[m]))
     
-    def tri_selectif(mon_tableau):
-        l= len(mon_tableau)
-        for i in range(0,l-1):
-            mini=i
-            for j in range(i+1,l):
-                if mon_tableau[j]<mon_tableau[mini]:
-                    mini=j
-            if mini != i:
-                (mon_tableau[i],mon_tableau[mini])=(mon_tableau[mini],mon_tableau[i])
-        return(mon_tableau)
-    
     tri_selectif(mon_tableau)
-    for element in mon_tableau:
-        print(element,end=' ')
-    print('')
