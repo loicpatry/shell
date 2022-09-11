4df2fdf902bdd9ce15327ca8c07f5d43bc17fd8e
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-11 14:24:51 +0200

    Update eau15.py

--- eau/eau15.py
+++ eau/eau15.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  8 11:23:59 2022
+Created on Sun Sep 11 12:38:07 2022
 
 @author: patry
 """
 import sys
-def tri_selectif(mon_tableau):
+def tri_selectif(mon_tableau): 
         l= len(mon_tableau)
         for i in range(0,l-1):
             mini=i
@@ -19,35 +19,41 @@ def tri_selectif(mon_tableau):
     
 def split(s):
     return [char for char in s]
-    
-l=len(sys.argv)
+
+#########################################
+
+l=len(sys.argv) ## transformer les arguments en liste
 arguments=[]
 for i in range(1,l):
     arguments.append(sys.argv[i])
 
-    
-for s in range(len(arguments)):
+#########################################
+
+for s in range(len(arguments)):  #transformer les arguments en liste de liste avec les letrres
     lol=arguments[s]
     arguments[s] = split(lol)
-
-for a in range(len(arguments)):
+    
+#########################################
+    
+for a in range(len(arguments)):  # transformer les arguments en liste de liste avec les numéros ASCII
     long2=len(arguments[a])
     for z in range(long2):
-        arguments[a][z]=ord(arguments[a][z])
-arguments_init=arguments
+        arguments[a][z]=str(ord(arguments[a][z]))
 
-#print(arguments_init)
-L=len(arguments)    
-mon_tableau_init=[]
-for b in range(0,L):
-    mon_tableau_init.append(arguments_init[b][0])
-mon_tableau=[]
-for b in range(0,L):
-    mon_tableau.append(arguments[b][0])
-tri_selectif(mon_tableau)
+arguments_init=[]
+for a in range(0,len(arguments)):
+    arguments_init.append(arguments[a])
 
-for g in range(0,L):
-    index=mon_tableau_init.index(mon_tableau[g])+1
-    print(sys.argv[index],end=' ')
+for i in range(len(arguments)-1,0,-1):
+    for j in range(0,i):
+        b=0
+        while arguments[j+1][b]==arguments[j][b]:
+            b=b+1
+        if arguments[j+1][b]<arguments[j][b]:
+            (arguments[j+1],arguments[j])=(arguments[j],arguments[j+1])
+            
+for a in range(0,len(arguments)):
+    index= arguments_init.index(arguments[a])
+    print(sys.argv[index+1],end=' ')
 print('')
-
+    
