2445b56a4c16a42e2bef8d48aafb3b3d060b49ca
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-09-11 21:19:31 +0200

    Update eau13.py

--- eau/eau13.py
+++ eau/eau13.py
@@ -6,7 +6,17 @@ Created on Thu Sep  8 09:46:43 2022
 @author: patry
 """
 import sys 
-
+#############################################################################
+def my_bubble_sort(mon_tableau):
+        for i in range(len(mon_tableau)-1,0,-1):
+            for j in range(0,i):
+                if mon_tableau[j+1]<mon_tableau[j]:
+                    (mon_tableau[j+1],mon_tableau[j])=(mon_tableau[j],mon_tableau[j+1])
+        for element in mon_tableau:
+            print(element,end=' ')
+        print('')
+    
+#############################################################################
 n=len(sys.argv)
 
 if n<3:
@@ -24,15 +34,5 @@ else:
     for m in range(1,n):
         mon_tableau.append(int(sys.argv[m]))
     
-    def my_bubble_sort(mon_tableau):
-        for i in range(len(mon_tableau)-1,0,-1):
-            for j in range(0,i):
-                if mon_tableau[j+1]<mon_tableau[j]:
-                    (mon_tableau[j+1],mon_tableau[j])=(mon_tableau[j],mon_tableau[j+1])
-        return(mon_tableau)
-    
     my_bubble_sort(mon_tableau)
-    for element in mon_tableau:
-        print(element,end=' ')
-    print('')
     
