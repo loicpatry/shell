99f33739597b27493f0b141a738cfb41bc3260f1
loicpatry <33187478+loicpatry@users.noreply.github.com>
2022-11-04 13:21:33 +0100

    Create feu07.py

---
+++ feu/feu07.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 16:04:01 2022
+@author: patry
+"""
+import time
+print('J\'ai terminé l\'épreuve du feu et c\'était')
+time.sleep(3)
+print('longggggggggggg')
